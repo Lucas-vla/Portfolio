c7b26819e7fdf448d584839c08e86a66f6eb5988
Lucas <lucasviola.pro@hotmail.com>
2024-05-27 16:20:02 +0200

    push

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,27 @@
-import { FaLinkedin } from "react-icons/fa"
-import { FaGithub } from "react-icons/fa"
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
-  return <nav className="mb-20 flex items-center justify-between py-6 ">
-   
-    <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin className="w-10 h-10"/>
-        <FaGithub className="w-10 h-10"/>
-    </div>
-  </nav>
-}
+  const handleClickLinkedIn = () => {
+    window.open("https://www.linkedin.com/in/lucas-viola1/", "_blank");
+  };
 
-export default Navbar
+  const handleClickGitHub = () => {
+    window.open("https://github.com/Lucas-vla", "_blank");
+  };
+
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a href="#" onClick={handleClickLinkedIn}>
+          <FaLinkedin className="w-10 h-10" />
+        </a>
+
+        <a href="#" onClick={handleClickGitHub}>
+          <FaGithub className="w-10 h-10" />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
