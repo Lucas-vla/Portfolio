18071612379f590748dad0119cad6c7cf1551a82
Lucas Viola <155719784+Lucas-vla@users.noreply.github.com>
2024-08-15 16:00:29 +0200

    ajout coder Monkeys

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,49 +2,36 @@ import project1 from "../assets/projects/formula1-logo.jpg";
 import project2 from "../assets/projects/nytime.jpg";
 import project3 from "../assets/projects/weatherApp.png";
 import project4 from "../assets/projects/maggy.jpg";
+import project5 from "../assets/projects/mymoviz.png";
+import project0 from "../assets/projects/singe.svg";
 
 export const HERO_CONTENT = `Je suis un développeur full stack passionné, spécialisé dans la création d'applications web et mobile robustes et évolutives. J'ai développé mes compétences dans les technologies front-end telles que React et Next.js, ainsi que dans les technologies back-end comme Node.js, Express.js et MongoDB. Actuellement, je suis à la recherche d'un stage pour faire mes premiers pas en entreprise dans ce domaine et mettre à profit mon experience pour développer des solutions innovantes qui stimulent la croissance des entreprises et offrent des expériences utilisateur exceptionnelles.`;
 
 export const ABOUT_TEXT = `Je suis un développeur full stack dévoué et polyvalent, passionné par la création d'applications web efficaces et performantes. J'ai étudié diverses technologies, dont React / Native, Next.js, Node.js, Expo Express.js et MongoDB. Mon parcours en développement web a commencé par une profonde curiosité pour le fonctionnement des choses ainsi qu'un potentiel créatif infini, et il a évolué en une reconversion où je m'efforce continuellement d'apprendre et de m'adapter à de nouveaux défis. Je m'épanouis dans des environnements collaboratifs et j'aime résoudre des problèmes complexes pour fournir des solutions de haute qualité. J'aime rester actif, explorer de nouvelles technologies et contribuer à de nouveaux projets.`;
 
-export const EXPERIENCES = [
-  {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  },
-  {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
-];
-
 export const PROJECTS = [
+  {
+    title: "Coders Monkey",
+    image: project0,
+    description:
+      "Projet réalisé lors d'une formation, recréant le site de la dite formation. Création de la connexion / inscription, Onboarding, Footer",
+    technologies: ["React", "Tailwind", "TypeScript", "Firebase"],
+    url: "https://project-corder-monkey-5aj7nsgfp-lucas-violas-projects.vercel.app/",
+  },
   {
     title: "Formula 1 App (Work in progress)",
     image: project1,
     description:
       "Application autour de la Formule 1 moderne et historique, Comprenant des fonctionnalités tel que l'authentification utilisateur, compte à rebour, détails des courses et Bio pilotes, integration des 'Highlight' vidéo, Random Fact...",
     technologies: ["HTML", "CSS", "React Native", "Node.js", "MongoDB", "Expo"],
+  },
+  {
+    title: "MyMoviz",
+    image: project5,
+    description:
+      "Application Web affichant les dernier films sortis recemment via une API, possibilité de Like, Noter, voir les avis des autres ainsi que la description du film",
+    technologies: ["React", "HTML", "CSS", "JavaScript"],
+    url: "https://mymoviespart5-frontend.vercel.app/",
   },
   {
     title: "My Morning News",
